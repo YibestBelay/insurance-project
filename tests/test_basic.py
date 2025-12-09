97d4538293e6c53a7e46558b2c131788378024e3
Yibest <yibestbelay@gmail.com>
2025-12-09 11:35:49 +0300

    CI/CD: Setup GitHub Actions with basic tests

---
+++ tests/test_basic.py
@@ -0,0 +1,10 @@
+﻿import pytest
+from src.data_preprocessing import load_data
+
+def test_load_data():
+    # This is a simple test that will pass
+    assert 1 + 1 == 2
+
+def test_sample():
+    # Another test
+    assert True
